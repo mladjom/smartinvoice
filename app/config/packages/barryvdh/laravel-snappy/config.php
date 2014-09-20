190cf6b3d6e6ae11245d3d3451851c8d2afeab4f
Mladjo <mladen@milentijevic.com>
2014-09-20 11:56:47 +0200

    wkhtmltopdf

--- app/config/packages/barryvdh/laravel-snappy/config.php
+++ app/config/packages/barryvdh/laravel-snappy/config.php
@@ -9,7 +9,7 @@ return array(
         'options' => array(),
     ),
     'image' => array(
-        'enabled' => false,
+        'enabled' => true,
         'binary' => base_path('vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage-amd64'),
         'options' => array(),
     ),
